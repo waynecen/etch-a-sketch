0fd2e8763da7ec3588c17868ba9248ff2e5f8410
Wayne <wayne.cen@gmail.com>
2021-11-23 00:04:49 -0500

    [WC] Add createDiv, resetDiv functions and css
    Started gridDiv() - uses nested loop to create a grid, divs with box class span horizontally and 'rows' generated vertically. Grid is only created if user inputs value lower or equal to 100.
    
    Added mouseover event listener to box so the background color of the div boxes change when hovered over.
    
    Created resetDiv(), attaches to a button with an onclick event. Updates num to take new grid size and removes previous grid, then calls gridDiv() again to start new grid.
    
    Added CSS to style button and grid, included font for header. Used SCSS experimentally to explore variables in CSS.

--- js/index.js
+++ js/index.js
@@ -1,15 +1,53 @@
 //script for etch-a-sketch
 const container = document.querySelector('#container');
 
-const content = document.createElement('div');
-content.classList.add('content');
-container.appendChild(content);
+let num = prompt('Enter grid size (max value-100):');
 
-const row = document.createElement('div');
-row.classList.add('row');
-content.appendChild(row);
+function gridDiv () {
+   
+        const content = document.createElement('div');
+        content.classList.add('content');
+        container.appendChild(content);
+        content.setAttribute('id','grid');
 
-const box = document.createElement('div');
-box.classList.add('box');
-box.textContent = 'boo';
-row.appendChild(box);
+        if (num <= 100) {
+        for (let e = 0; e < num; e++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        content.appendChild(row);
+    
+            for (let i = 0; i < num; i++) {
+                const box = document.createElement('div');
+                box.classList.add('box');
+                box.style.display = 'flex';
+                
+                let boxWidth = 960/num;
+                box.style.width = boxWidth + "px";
+                let boxHeight = 960/num;
+                box.style.height = boxHeight + "px";
+
+                box.addEventListener('mouseover', () => {
+                    box.style.background = '#1C1C1C';
+                    box.style.transition = '0ms';
+                })
+
+                row.appendChild(box);
+
+            }
+        }
+    }
+}
+
+function resetDiv() {
+    let newGrid = prompt("Enter size of new grid (max value-100):");
+    num = newGrid;
+    let grid = document.getElementById('grid');
+    if (grid == null) {
+        return;
+    } else {
+        grid.remove();
+    }
+    gridDiv();
+}
+
+gridDiv();
